e28c7c180c717b30d30832e8c5b4313a63745f55
Varshil <varshil247@gmail.com>
2024-02-28 00:04:55 +0000

    initial setup

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      {/* hero section */}
+      <section className="section">
+        <h2>Varshil Patel</h2>
+      </section>
+
+      {/* about section */}
+      <section className="section">
+        <h2>About me</h2>
+      </section>
+
+      {/* project section */}
+      <section className="section">
+        <h2>My Projects</h2>
+      </section>
+
+      {/* contact section */}
+      <section className="section">
+        <h2>Contact Me</h2>
+      </section>
+    </div>
+  );
+}
+
+export default App;
